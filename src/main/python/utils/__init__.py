2ca7f9dcb00e2bdb6041df4c088bdf2a4dfa5af8
danglotb <benjamin.danglot@davidson.fr>
2021-04-15 11:56:05 +0200

    remove old file to create an archive

--- src/main/python/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .run_args import *
-from .cmd_utils import *
-from .data_args import *
-from .graph_args import *
-from .json_utils import *
-from .readme_args import *
-
-__version__ = '0.0.1'
