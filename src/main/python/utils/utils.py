c54e5d9983748ea4279075799ff3078824f242a6
danglotb <benjamin.danglot@davidson.fr>
2021-10-21 12:24:18 +0200

    feat: add get commit id and sort

--- src/main/python/utils/utils.py
+++ src/main/python/utils/utils.py
@@ -1,5 +1,6 @@
+import os
 
-def check_if_end_properly(fileList):
+def check_if_end_properly(fileList, dirName):
     for file in fileList:
         if file == 'end.txt':
             return False
@@ -7,3 +8,11 @@ def check_if_end_properly(fileList):
 
 def get_id_commit_function(commit_path):
     return int(commit_path.split('/')[4].split('_')[0])
+
+def get_considered_commits_and_sort(root_folder):
+    considered_commits = []
+    for dirName, subdirList, fileList in os.walk(root_folder):
+        if dirName.endswith('diff-jjoules'):
+            if check_if_end_properly(fileList, dirName):
+                considered_commits.append(dirName)
+    return sorted(considered_commits, key=get_id_commit_function)
