cd8f2739c2b7d0f70d26ebe00c8a21f91e02e15b
danglotb <benjamin.danglot@davidson.fr>
2022-02-24 13:57:29 +0100

    exp: use now 100 iterationsé

--- src/main/python/utils/cmd/mvn_cmd.py
+++ src/main/python/utils/cmd/mvn_cmd.py
@@ -59,7 +59,7 @@ def mvn_diff_jjoules_no_mark(
             OPT_REPO_V1 + path_first_repository,
             OPT_REPO_V2 + path_second_repository,
             OPT_NO_REPORT,
-            OPT_ITERATION + '2',
+            OPT_ITERATION + '100',
             OPT_MEASURE,
             '>' + output_path_file,
             '2>&1'
