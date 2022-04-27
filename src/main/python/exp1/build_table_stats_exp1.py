4e11f438ae11b12bfc46eadac47d63c75e92b852
danglotb <benjamin.danglot@davidson.fr>
2022-04-27 14:22:03 +0200

    update script exp1 to build table and distr

--- src/main/python/exp1/build_table_stats_exp1.py
+++ src/main/python/exp1/build_table_stats_exp1.py
@@ -17,16 +17,11 @@ from utils.statitics import *
 from utils.cmd.latex_cmd import *
 from utils.cmd.graph_cmd import *
 
-global nb_record_removed
-
-def compute_med_and_var(diff_jjoules_directory, data, units, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec):
-    global nb_record_removed
+def compute_med_and_var(diff_jjoules_directory, data, units, medianes_per_unit):
     count_test, count_sec_gt_instr, count_sec_gt_cycles = 0, 0, 0 
+    energy_cvs, instr_cvs, cycles_cvs = [], [], []
     for test in data:
         energy_data, instr_data, cycles_data = from_dict_to_array_rm_zero_for_keys_array(data[test], units)
-        for d in data[test]:
-            if d[units[0]] == 0 or d[units[1]] == 0 or d[units[2]] == 0:
-                nb_record_removed = nb_record_removed + 1
         if len(energy_data) == 0 or len(instr_data) == 0 or len(cycles_data) == 0:
             #if not diff_jjoules_directory.split('/')[4] in directory:
                 #print(diff_jjoules_directory.split('/')[4])
@@ -38,77 +33,49 @@ def compute_med_and_var(diff_jjoules_directory, data, units, medianes_per_unit, 
         medianes_per_unit[ENERGY_KEY].append(energy_med)
         medianes_per_unit[INSTR_KEY].append(instr_med)
         medianes_per_unit[CYCLES_KEY].append(cycles_med)
+        energy_cvs.append(energy_cv)
+        instr_cvs.append(instr_cv)
+        cycles_cvs.append(cycles_cv)
         count_sec_gt_instr = count_sec_gt_instr  + (1 if energy_cv > instr_cv else 0)
         count_sec_gt_cycles = count_sec_gt_cycles  + (1 if energy_cv > cycles_cv else 0)
-        #diff_sec_cycles.append(cycles_cv / energy_cv)
-        #diff_sec_instr.append(instr_cv / energy_cv)
-        #diff_sec_cycles.append(energy_cv / cycles_cv)
-        #diff_sec_instr.append(energy_cv / instr_cv)
-        #diff_sec_cycles.append(cycles_cv)
-        #diff_sec_instr.append(instr_cv)
-        #diff_sec_sec.append(energy_cv)
-        #diff_sec_cycles.append(cycles_qcd)
-        #diff_sec_instr.append(instr_qcd)
-        #diff_sec_sec.append(energy_qcd)
-        diff_sec_cycles.append(cycles_rel_avg_dev)
-        diff_sec_instr.append(instr_rel_avg_dev)
-        diff_sec_sec.append(energy_rel_avg_dev)
         count_test = count_test + 1
-    return count_test, count_sec_gt_instr, count_sec_gt_cycles, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec
+    return count_test, count_sec_gt_instr, count_sec_gt_cycles, medianes_per_unit, energy_cvs, instr_cvs, cycles_cvs
 
-def compute_stats_for_commit(diff_jjoules_directory, units, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec):
+def compute_stats_for_commit(diff_jjoules_directory, units, medianes_per_unit):
     id_commit = get_id_commit_function(diff_jjoules_directory)
     data_V1 = read_json(diff_jjoules_directory + '/' + DATA_V1_JSON_FILE_NAME)
     data_V2 = read_json(diff_jjoules_directory + '/' + DATA_V2_JSON_FILE_NAME)
-    nb_test_v1, count_sec_gt_instr_v1, count_sec_gt_cycles_v1, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec = compute_med_and_var(diff_jjoules_directory + '/' + DATA_V1_JSON_FILE_NAME, data_V1, units, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec)
-    nb_test_v2, count_sec_gt_instr_v2, count_sec_gt_cycles_v2, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec = compute_med_and_var(diff_jjoules_directory + '/' + DATA_V2_JSON_FILE_NAME, data_V2, units, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec)
-    return nb_test_v1 + nb_test_v2, count_sec_gt_instr_v1 + count_sec_gt_instr_v2, count_sec_gt_cycles_v1 + count_sec_gt_cycles_v2, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec
+    nb_test_v1, count_sec_gt_instr_v1, count_sec_gt_cycles_v1, medianes_per_unit, energy_cv_v1, instr_cv_v1, cycles_cv_v1 = compute_med_and_var(diff_jjoules_directory + '/' + DATA_V1_JSON_FILE_NAME, data_V1, units, medianes_per_unit)
+    nb_test_v2, count_sec_gt_instr_v2, count_sec_gt_cycles_v2, medianes_per_unit, energy_cv_v2, instr_cv_v2, cycles_cv_v2 = compute_med_and_var(diff_jjoules_directory + '/' + DATA_V2_JSON_FILE_NAME, data_V2, units, medianes_per_unit)
+    return nb_test_v1 + nb_test_v2, count_sec_gt_instr_v1 + count_sec_gt_instr_v2, count_sec_gt_cycles_v1 + count_sec_gt_cycles_v2, medianes_per_unit, energy_cv_v1 + energy_cv_v2, instr_cv_v1 + instr_cv_v2, cycles_cv_v1 + cycles_cv_v2
 
 def build_row_for_project(project, project_root_folder):
-    global nb_record_removed
-    nb_record_removed = 0
     commits = []
     medianes_per_unit = {}
     units = [ENERGY_KEY, INSTR_KEY, CYCLES_KEY]
-    diff_sec_instr, diff_sec_cycles, diff_sec_sec = [], [], []
     for unit in units:
         medianes_per_unit[unit] = []
     nb_test, nb_applicable_commit, count_sec_gt_instr, count_sec_gt_cycles = 0, 0, 0, 0
+    energy_cvs, instr_cvs, cycles_cvs = [], [], []
     properly_ended = []
     commit_folders = listdir(project_root_folder)
     commit_folders.sort(key=lambda commit_folder: int(commit_folder.split('_')[0]) if commit_folder != 'rq2' else -1)
     for commit_folder in commit_folders:
-        if commit_folder == 'rq2':
-            continue
         if len(properly_ended) == 100:
-            corrcoef_instr = corrcoef_datas(medianes_per_unit[ENERGY_KEY], medianes_per_unit[INSTR_KEY])
-            corrcoef_cycles = corrcoef_datas(medianes_per_unit[ENERGY_KEY], medianes_per_unit[CYCLES_KEY])
-            to_be_printed = [
-                project,
-                str(len(commits)) + ('(' + compute_and_format_perc(len(commits), nb_applicable_commit) + ')'),
-                str(nb_test),
-                format_int(nb_test, count_sec_gt_instr),
-                format_int(nb_test, count_sec_gt_cycles),
-                #str(nb_record_removed),
-                '{:.2f}'.format(corrcoef_instr[0][1]),
-                '{:.2f}'.format(corrcoef_cycles[0][1])
-            ]
-            nb_record_removed = 0
-            print(to_row_latex(to_be_printed))
-            return properly_ended, diff_sec_instr, diff_sec_cycles, diff_sec_sec
-        #print(project, commit_folder, len(properly_ended), '/', 100)
+            break
         commits.append(commit_folder)
-        diff_jjoules_directory = root_folder + '/' + commit_folder + '/diff-jjoules'
+        diff_jjoules_directory = project_root_folder + '/' + commit_folder + '/diff-jjoules'
         end_properly = check_if_end_properly(diff_jjoules_directory)
         if end_properly:
             properly_ended.append(commit_folder)
-            current_nb_test, current_count_sec_gt_instr, current_count_sec_gt_cycles, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec = compute_stats_for_commit(diff_jjoules_directory, units, medianes_per_unit, diff_sec_instr, diff_sec_cycles, diff_sec_sec)
+            current_nb_test, current_count_sec_gt_instr, current_count_sec_gt_cycles, medianes_per_unit, energy_cv, instr_cv, cycles_cv = compute_stats_for_commit(diff_jjoules_directory, units, medianes_per_unit)
+            energy_cvs.extend(energy_cv)
+            instr_cvs.extend(instr_cv)
+            cycles_cvs.extend(cycles_cv)
             nb_test = nb_test + current_nb_test
             count_sec_gt_cycles = count_sec_gt_cycles + current_count_sec_gt_cycles
             count_sec_gt_instr = count_sec_gt_instr + current_count_sec_gt_instr
             nb_applicable_commit = nb_applicable_commit + 1
-        #else:
-        #   print(commit_folder)
     corrcoef_instr = corrcoef_datas(medianes_per_unit[ENERGY_KEY], medianes_per_unit[INSTR_KEY])
     corrcoef_cycles = corrcoef_datas(medianes_per_unit[ENERGY_KEY], medianes_per_unit[CYCLES_KEY])
     print(len(properly_ended), project)
@@ -116,15 +83,15 @@ def build_row_for_project(project, project_root_folder):
         project,
         str(len(commits)) + ('(' + compute_and_format_perc(len(commits), nb_applicable_commit) + ')'),
         str(nb_test),
+        '{:.2f}'.format(mediane(energy_cvs)),
+        '{:.2f}'.format(mediane(instr_cvs)),
+        '{:.2f}'.format(mediane(cycles_cvs)),
         format_int(nb_test, count_sec_gt_instr),
         format_int(nb_test, count_sec_gt_cycles),
-        #str(nb_record_removed),
         '{:.2f}'.format(corrcoef_instr[0][1]),
         '{:.2f}'.format(corrcoef_cycles[0][1])
     ]
-    nb_record_removed = 0
     print(to_row_latex(to_be_printed))
-    return properly_ended, diff_sec_instr, diff_sec_cycles, diff_sec_sec
 
 if __name__ == '__main__':
 
@@ -135,26 +102,17 @@ if __name__ == '__main__':
             'Project',
             '\#Commits',
             '\#TestExec',
+            '$\\tilde{x}_{SEC}$',
+            '$\\tilde{x}_{instr}$',
+            '$\\tilde{x}_{SEC}$',
             '\#$CV(SEC) > CV(instr)$',
             '\#$CV(SEC) > CV(cycles)$',
             '$\\rho_{instr}$',
             '$\\rho_{cycles}$',
         ])
     )
-    properly_ended = []
-    diff_sec_instr_per_project, diff_sec_cycles_per_project, diff_sec_sec_per_project= {}, {}, {}
+
     for project in PROJECTS:
-        root_folder = args.output + project
-        properly_ended_project, diff_sec_instr, diff_sec_cycles, diff_sec_sec = build_row_for_project(project, root_folder)
-        properly_ended.append(properly_ended_project)
-        diff_sec_instr_per_project[project] = diff_sec_instr
-        diff_sec_cycles_per_project[project] = diff_sec_cycles
-        diff_sec_sec_per_project[project] = diff_sec_sec
+        root_folder = args.output + project + '/exp1'
+        build_row_for_project(project, root_folder)
     print(to_footer_latex())
-
-    jitter_plot(PROJECTS, [diff_sec_instr_per_project, diff_sec_cycles_per_project, diff_sec_sec_per_project], ['instr', 'cycles', 'sec'])
-
-    '''
-    for e in properly_ended:
-        print(e)
-    '''
---
+++ src/main/python/exp1/draw_distribution_exp1.py
@@ -0,0 +1,82 @@
+import sys
+import os
+
+from os import listdir
+
+SCRIPT_DIR = os.path.abspath('./src/main/python/utils/')
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from utils.cmd.git_cmd import *
+from utils.cmd.io_cmd import *
+from utils.cmd.mvn_cmd import *
+from utils.cmd.json_cmd import *
+from utils.constants import *
+from utils.args.build_table_exp1_args import *
+from utils.utils import *
+from utils.statitics import *
+from utils.cmd.latex_cmd import *
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+def get_cvs(data):
+    units = [ENERGY_KEY, INSTR_KEY, CYCLES_KEY]
+    cvs_cycles = []
+    cvs_energies = []
+    cvs_instr = []
+    for test in data:
+        energy_data, instr_data, cycles_data = from_dict_to_array_rm_zero_for_keys_array(data[test], units)
+        if len(energy_data) == 0:
+            print(test)
+            continue
+        energy_med, energy_variance, energy_stddev, energy_cv, energy_qcd, energy_avg_abs_dev, energy_rel_avg_dev = stats(energy_data)
+        instr_med, instr_variance, instr_stddev, instr_cv, instr_qcd, instr_avg_abs_dev, instr_rel_avg_dev = stats(instr_data)
+        cycles_med, cycles_variance, cycles_stddev, cycles_cv, cycles_qcd, cycles_avg_abs_dev, cycles_rel_avg_dev = stats(cycles_data)
+        cvs_cycles.append(cycles_cv)
+        cvs_energies.append(energy_cv)
+        cvs_instr.append(instr_cv)
+    return cvs_energies, cvs_instr, cvs_cycles
+
+def draw_distribution(project, project_root_folder):
+    cvs_cycles = []
+    cvs_energies = []
+    cvs_instr = []
+    for commit_folder in listdir(project_root_folder):
+        diff_jjoules_directory = root_folder + '/' + commit_folder + '/diff-jjoules'
+        end_properly = check_if_end_properly(diff_jjoules_directory)
+        if end_properly:
+            data_v1 = read_json(diff_jjoules_directory + DATA_V1_JSON_FILE_NAME)
+            data_v2 = read_json(diff_jjoules_directory + DATA_V2_JSON_FILE_NAME)
+            energy_cv, instr_cv, cycles_cv = get_cvs(data_v1)
+            cvs_cycles.extend(cycles_cv)
+            cvs_energies.extend(energy_cv)
+            cvs_instr.extend(instr_cv)
+            energy_cv, instr_cv, cycles_cv = get_cvs(data_v2)
+            cvs_cycles.extend(cycles_cv)
+            cvs_energies.extend(energy_cv)
+            cvs_instr.extend(instr_cv)
+    '''
+    kwargs = dict(alpha=0.5, bins=100, density=True, stacked=True)
+    plt.hist(cvs_energies, **kwargs,  color='g', label='SEC')
+    plt.hist(cvs_instr, **kwargs,  color='b', label='Instr')
+    plt.hist(cvs_cycles, **kwargs,  color='r', label='Cycles')
+    '''
+    kwargs = dict(hist_kws={'alpha':.6}, kde_kws={'linewidth':2})
+
+    plt.figure(figsize=(10,7), dpi= 80)
+    sns.distplot(cvs_energies, color="dodgerblue", label="SEC", **kwargs)
+    sns.distplot(cvs_instr, color="orange", label="Instr", **kwargs)
+    sns.distplot(cvs_cycles, color="deeppink", label="Cycles", **kwargs)
+    plt.legend()
+    plt.show()
+    plt.clf()
+
+if __name__ == '__main__':
+
+    args = RunArgs().build_parser().parse_args()
+    sns.set_style("white")
+
+    for project in PROJECTS:
+        root_folder = args.output + project + '/exp1'
+        draw_distribution(project, root_folder)
